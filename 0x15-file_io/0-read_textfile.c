cb8038cbabf785f057295bcd21c25b18c3501975
Clive_Sasaka <clivesasaka@gmail.com>
2022-05-03 07:43:46 -0700

    Input and output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+  *read_textfile -  reads a text file and prints it
+  *@filename: file name
+  *@letters: letter input
+  *Return: actual number of letters it could read and print
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+ssize_t n_read, n_wrote;
+char *buffer;
+if (filename == NULL)
+return (0);
+if (filename == NULL)
+return (0);
+
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+
+n_read = read(fd, buffer, letters);
+if (n_read == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+
+n_wrote = write(STDOUT_FILENO, buffer, n_read);
+if (n_wrote == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+close(fd);
+return (n_read);
+}
