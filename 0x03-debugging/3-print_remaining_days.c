589102c1596d4bd5f9c2f6ac7c0e1f9bbb4dd12d
Clive_Sasaka <clivesasaka@gmail.com>
2022-07-18 12:35:04 -0700

    Debug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -26,12 +26,20 @@ else
 {
 if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+printf("Day of the year: %d\n", day - 31);
+}
+
+else
+{
+if (month == 2 && day != 60) || (day > 31 && day <= 59)
+{
+printf("Day of the year: %d\n", day - 31);
 }
 else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
+}
 }
 }
 }
